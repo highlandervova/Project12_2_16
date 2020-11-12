724994ef78af5d89583851bfe297a8e2e2fefe2b
pani-marinka <pani_marinka@ukr.net>
2020-11-12 22:20:39 +0200

    initPanarina

---
+++ src/main/MinMaxSorter.java
@@ -0,0 +1,27 @@
+package main;
+
+public class MinMaxSorter implements Sorter{
+    @Override
+    public int[] sort(int[] arr) {
+
+            int k,j;
+
+         int[] tmp = new int[arr.length];
+
+         for (int i = 0; i < arr.length; i++) {
+             k=arr[0];
+             j=0;
+                for (int n = 1; n < arr.length; n++) {
+                 if (k > arr[n]) {
+                    k = arr[n];
+                    j = n;
+                 }
+                }
+            tmp[i]=k;
+            arr[j]=Integer.MAX_VALUE;
+            }
+
+        arr=tmp;
+        return arr;
+         }
+}
